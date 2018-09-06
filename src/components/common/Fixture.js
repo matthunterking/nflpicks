6559cd4224b62b8191e2336bd7073b6b338647a6
matthunterking <matthew85king@gmail.com>
2018-09-06 19:18:50 +0100

    Added styling to pick buttons and removed the teams component

--- src/components/common/Fixture.js
+++ src/components/common/Fixture.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import Team from '../common/Team';
 
 const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
 
-  //has one of the other fixtures been locked?
-  // console.log('Has one of the other fixtures been locked?',  locked && picks.filter(pick => pick.lock).filter(pick => pick.gameId === fixture.id.toString()).length);
-  // console.log('in the fixture component fixture',  picks.filter(pick => pick.lock));
-
-  // const pickActive = !picks.filter(pick => pick.gameId === fixture.id.toString()).length;
-  // const lockActive = !picks.filter(pick => pick.gameId === fixture.id.toString() && pick.lock).length;
-  // const selectedTeam = !pickActive ? picks.filter(pick => pick.gameId === fixture.id.toString())[0].winnerPick : '';
-  // console.log('this is a fixture', pickActive, lockActive, selectedTeam);
-
-
-//TODO: 1. MAKE AWAY BUTTON A FUNCTION TO DECIDE WHAT BUTTON NEEDS TO BE THERE
 //TODO: 2. MAKE A HANDLE UNLOCK FEATURE
 
   const otherLocked = locked && !picks.filter(pick => pick.lock).filter(pick => pick.gameId === fixture.id.toString()).length;
@@ -29,21 +17,22 @@ const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
   const awayLocked = locked === fixture.awayTeam._id.toString();
   const homeLocked = locked === fixture.homeTeam._id.toString();
 
-  if(fixture._id.toString() === '5b841cb1a5b13ebb36eeeca5') {
-    console.log('awayPicked', awayPicked, 'homePicked', homePicked, 'otherLocked', otherLocked, 'thisLocked', thisLocked, 'availableToLockAway', availableToLockAway, 'availableToLockHome', availableToLockHome, 'awayLocked', awayLocked);
-  }
   return (
     <div className="fixtureContainer">
       {(!awayPicked && !thisLocked) && <button
         onClick={handleClick}
         game={fixture.id}
         winner={fixture.awayTeam._id}
-        loser={fixture.homeTeam._id}>
+        loser={fixture.homeTeam._id}
+        className="PickButton FixtureButton standardText button"
+      >
         Pick
       </button>}
 
-      {awayPicked && !awayLocked && (!availableToLockAway || otherLocked) && <button>
-        Picked!
+      {awayPicked && !awayLocked && (!availableToLockAway || otherLocked) && <button
+        className="inactive FixtureButton standardText button"
+      >
+        Picked
       </button>}
 
 
@@ -51,7 +40,9 @@ const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
         onClick={handleLock}
         game={fixture.id}
         winner={fixture.awayTeam._id}
-        loser={fixture.homeTeam._id}>
+        loser={fixture.homeTeam._id}
+        className="LockButton FixtureButton standardText button"
+      >
         Lock
       </button>}
 
@@ -59,8 +50,9 @@ const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
         onClick={handleClick}
         game={fixture.id}
         winner={fixture.awayTeam._id}
-        loser={fixture.homeTeam._id}>
-        UnLock
+        loser={fixture.homeTeam._id}
+        className="inactive FixtureButton standardText button"
+      >
       </button>}
 
       {homePicked && homeLocked && <button
@@ -71,16 +63,23 @@ const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
         Unlock and pick
       </button>}
 
-      <Team team={fixture.awayTeam} />
+      <div>
+        <p>{fixture.awayTeam.name}</p>
+      </div>
+
       <div>AT</div>
-      <Team team={fixture.homeTeam} />
+      <div>
+        <p>{fixture.homeTeam.name}</p>
+      </div>
 
 
       {(!homePicked && !thisLocked) && <button
         onClick={handleClick}
         game={fixture.id}
         winner={fixture.homeTeam._id}
-        loser={fixture.awayTeam._id}>
+        loser={fixture.awayTeam._id}
+        className="PickButton FixtureButton standardText button"
+      >
         Pick
       </button>}
 
@@ -93,7 +92,9 @@ const Fixture = ({ fixture, handleClick, handleLock, picks, locked, user }) => {
         onClick={handleLock}
         game={fixture.id}
         winner={fixture.homeTeam._id}
-        loser={fixture.awayTeam._id}>
+        loser={fixture.awayTeam._id}
+        className="LockButton FixtureButton standardText button"
+      >
         Lock
       </button>}
 
